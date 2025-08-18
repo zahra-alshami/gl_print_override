9b4482a7d8f7c08138a1741c7ccffcacf49f4789
zahra-alshami <zahrash.95@outlook.com>
2025-08-18 13:13:18 +0300

    fix filters

--- gl_print_override/overrides/printing.py
+++ gl_print_override/overrides/printing.py
@@ -1,4 +1,5 @@
 import frappe
+import json
 from frappe.utils.print_format import report_to_pdf as _orig_report_to_pdf
 
 @frappe.whitelist()
@@ -10,11 +11,29 @@ def report_to_pdf(*args, **kwargs):
     clean_kwargs = dict(kwargs)
 
     if clean_kwargs.get("report_name") == "General Ledger":
-        context = {**clean_kwargs, "_": frappe._}  # ✅ add translation helper
+        # Parse filters safely
+        filters = {}
+        if clean_kwargs.get("filters"):
+            try:
+                filters = (
+                    json.loads(clean_kwargs["filters"])
+                    if isinstance(clean_kwargs["filters"], str)
+                    else clean_kwargs["filters"]
+                )
+            except Exception:
+                filters = {}
+
+        # Build context for Jinja template
+        context = {
+            **clean_kwargs,
+            "filters": filters,   # ✅ now template sees "filters"
+            "_": frappe._         # ✅ translation helper
+        }
+
         clean_kwargs["html"] = frappe.render_template(
             "gl_print_override/templates/print_formats/gl_custom.html",
             context
         )
 
-    # ✅ returns raw PDF bytes, Frappe will wrap it into Response automatically
+    # Return PDF bytes (Frappe will handle Response)
     return _orig_report_to_pdf(*args, **clean_kwargs)
