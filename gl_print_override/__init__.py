3ee09dfb6d9745479481ff475037ef173558b50e
zahra-alshami <zahrash.95@outlook.com>
2025-08-31 11:05:59 +0300

    ..

--- gl_print_override/__init__.py
+++ gl_print_override/__init__.py
@@ -1,3 +1,4 @@
 # import frappe
 # import report_to_pdf
-frappe.utils.print_format.report_to_pdf =gl_print_override.overrides.printing.report_to_pdf
+import gl_print_override.overrides.printing
+# frappe.utils.print_format.report_to_pdf =gl_print_override.overrides.printing.report_to_pdf
